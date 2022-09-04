5e41e6d0320e6a620abec0c7f53c1f3bb21d3485
Tobias <git@tobis-page.de>
2022-09-04 18:47:28 +0200

    Added basic test to verify fit file is actually legal

--- README.md
+++ README.md
@@ -6,6 +6,40 @@ Currently intented for Course-Files but could be extended to other types if desi
 
 ... TODO ... !
 
+### "Low" level API
+
+```
+    import { FITEncoder } from './encoder.js';
+    // or include dist/fit-file-writer.js and just use the global object
+
+
+    const encoder = new FITEncoder();
+    // start file
+    encoder.writeFileId({ type: 'course', time_created: now.getTime() });
+
+    encoder.writeCourse({ name: 'My Course' });
+    // FIT files may contain multiple laps
+    // see src/mesg.js for possible values
+    encoder.writeLap({ ... });
+    encoder.writeRecord({ ... });
+    ...
+
+    // get result
+    const result = encode.blob;
+```
+
+### data types
+
+* pass timestamps as normal Javascript milliseconds stamps
+* they will be converted to FIT-style stamps (in seconds)
+* Note: the FIT epoch starts at 1989-12-31T00:00 underflows will result in 
+  values > year 2058 when reading
+
+* pass normal lat/long values FIT uses semicircles so some rounding errors may 
+  occur
+
+* distance is in meters
+
 ## License
 
 Licensed under the [MIT License (MIT)](LICENSE)
--- src/encoder.test.js
+++ src/encoder.test.js
@@ -1,9 +1,105 @@
-
-
 import QUnit from 'qunit';
+// Assert binary result is hard ... we simply parse the result
+// again and validate that :)
+import FitParser from 'fit-file-parser';
+
 import { FITEncoder } from './encoder.js';
 
-QUnit.test('simple course', function(assert) {
-    assert.equal(true, true);
-});
+QUnit.assert.close = function(number, expected) {
+    const error = 0.00001;
+    this.pushResult({
+	result: number === expected || (number < expected + error && number > expected - error),
+	actual: number,
+	expected: expected
+    });
+}
 
+QUnit.test('simple course', async function(assert) {
+    const now = new Date();
+    now.setMilliseconds(0);
+    const future = new Date();
+    future.setTime(now.getTime()+10*1000);
+
+    const encoder = new FITEncoder();
+    const points = [
+	[1,2], [3,4]
+    ];
+
+    encoder.writeFileId({ type: 'course', time_created: now.getTime() });
+    encoder.writeCourse({ name: 'My Course' });
+    encoder.writeLap({
+	timestamp: now.getTime(),
+	total_timer_time: 60,
+	start_time: now.getTime(),
+	start_position_lat: points[0][0],
+	start_position_long: points[0][1],
+	end_position_lat: points[1][0],
+	end_position_long: points[1][1],
+    });
+    // warum hat der erste record im ergebnis mehr werte?
+    encoder.writeRecord({
+	timestamp: now.getTime(),
+	position_lat: points[0][0],
+	position_long: points[0][1],
+    });
+    encoder.writeRecord({
+	timestamp: future,
+	position_lat: points[1][0],
+	position_long: points[1][1],
+    });
+    encoder.writeEvent({
+	timestamp: future,
+	event: 'timer',
+	event_type: 'stop_disable_all',
+	event_group: 0,
+    });
+
+    const parser = new FitParser.default({force: false});
+    const blob = encoder.blob;
+
+    assert.equal(blob.size, 177);
+
+    parser.parse(await blob.arrayBuffer(), function (error, data) {
+	assert.step('parsed');
+	if (error) {
+	    assert.notOk(true, `Error Parsing Result: ${error}`);
+	    return;
+	}
+	assert.deepEqual(data.file_ids,
+	    [{ type: 'course', time_created: now }]
+	);
+	assert.deepEqual(data.course.name, 'My Course');
+
+	assert.equal(data.laps.length, 1);
+	const lap = data.laps[0];
+	assert.equal(String(lap.timestamp), String(now));
+	assert.equal(String(lap.start_time), String(now));
+	assert.equal(lap.total_timer_time, 60);
+	assert.close(lap.start_position_lat, points[0][0]);
+	assert.close(lap.start_position_long, points[0][1]);
+	assert.close(lap.end_position_lat, points[1][0]);
+	assert.close(lap.end_position_long, points[1][1]);
+
+	assert.equal(data.records.length, 2);
+	let record = data.records[0];
+	assert.equal(String(record.timestamp), String(now));
+	assert.close(record.position_lat, points[0][0]);
+	assert.close(record.position_long, points[0][1]);
+	// Note: fit-file-parser also returns
+	//  elapsed_time: 0, timer_time: 0
+	// that's a bonus feature of the lib and NOT part of the FIT-file
+
+	record = data.records[1];
+	assert.equal(String(record.timestamp), String(future));
+	assert.close(record.position_lat, points[1][0]);
+	assert.close(record.position_long, points[1][1]);
+
+	assert.deepEqual(data.events, [{
+	    timestamp: future,
+	    event: 'timer',
+	    event_type: 'stop_disable_all',
+	    event_group: 0
+	}]);
+    });
+    assert.verifySteps(['parsed']);
+});
